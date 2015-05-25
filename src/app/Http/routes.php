0eb5e2e6a86a122666f189940e1beacfe3ce2e11
Refik ONAL <refikonal@gmail.com>
2015-05-25 15:09:47 +0300

    Add crsf token to response
    To make it easier to test using PAW

--- src/app/Http/routes.php
+++ src/app/Http/routes.php
@@ -1,5 +1,10 @@
 <?php
 
 Route::get('/', function(){
-    return response()->view('welcome');
+    return response()->view('welcome')->header('X-CSRF-TOKEN', csrf_token());
 });
+
+Route::post('/', 'HashController@create');
+Route::get('/{hash}', 'HashController@redirect');
+Route::put('/{hash}', 'HashController@update');
+
