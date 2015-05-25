4d5a539b4f81c0d8c036b8cc9ac2a5cd8ec8fd61
Refik ONAL <refikonal@gmail.com>
2015-05-25 09:59:01 +0300

    Create a route and view to test

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,48 +1,13 @@
-<html>
-	<head>
-		<title>Laravel</title>
-		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>welcome - tin.st</title>
+</head>
+<body>
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
+    <h3>Welcome, stanger.</h3>
+    <p>I'm not ready to serve anyone yet. Feel free to visit later to see what's this site about. Cheers then.</p>
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
-
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
-			</div>
-		</div>
-	</body>
+</body>
 </html>
