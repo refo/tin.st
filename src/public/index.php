afdd2fe83b2a27c344fc1a27e6d3d4c1caffddc8
Refik ONAL <refikonal@gmail.com>
2015-05-26 18:31:43 +0300

    Add CORS headers

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,10 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+//header("Access-Control-Expose-Headers: ");
+header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, PATCH");
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
