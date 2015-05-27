185473c3f735748566f3ebac56a4e0c86f2528e8
Refik ONAL <refikonal@gmail.com>
2015-05-27 10:41:35 +0300

    Add "url" mutator

--- src/app/Slug.php
+++ src/app/Slug.php
@@ -1,6 +1,7 @@
 <?php namespace App;
 
 use Eloquent;
+use \Exception;
 
 class Slug extends Eloquent {
 
@@ -11,6 +12,25 @@ class Slug extends Eloquent {
     protected $attributes = [
         'visit' => 0,
     ];
+
+
+    public function setUrlAttribute($value)
+    {
+        $url = parse_url($value);
+
+        if ($url === FALSE) {
+            throw new Exception("Hata. URL ayrıştırılamadı.", 1);
+        }
+
+        $scheme   = isset($url['scheme'])   ? $url['scheme'].'://' : 'http://';
+        $host     = isset($url['host'])     ? $url['host']         : '';
+        $port     = isset($url['port'])     ? ':'.$url['port']     : '';
+        $path     = isset($url['path'])     ? $url['path']         : '';
+        $query    = isset($url['query'])    ? '?'.$url['query']    : '';
+        $fragment = isset($url['fragment']) ? '#'.$url['fragment'] : '';
+
+        $this->attributes['url'] = $scheme.$host.$port.$path.$query.$fragment;
+    }
 
 }
     
